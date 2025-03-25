44a8dba6c506c327e46d0c2fe96b50e6bf380588
SalmanAbiri <red.salman.abiri@gmail.com>
2025-03-25 16:58:01 +0330

    define PMT social class

--- temp.py
+++ temp.py
@@ -15,6 +15,51 @@ import random
 
 # 1. defining a class for usres
 # 1.1. agents class
+
+class PMTSubSocialParameters:
+    welfare = random.random() # variable
+    groundwaterStatus = random.random() # variable
+    trustInAuthorities = random.random()
+    agriculturalExperience = random.random()
+    education = random.random()
+    importanceAndUnderstandingOfConsequencesOfSubsidence = random.random()
+    Age = random.random()
+    dependenceOnAgriculturalLivelihoods = random.random()
+
+class PMTSocialParameters:
+        attitude = 0.0
+        subjectiveNorm = 0.0
+        preceivedBehavioralControl = 0.0
+        pmtSocialFactor = 0.0
+        def calculateParameters(self, pmtSubSocialParams):
+            for i in range(len(pmtSubSocialParams)):
+                sumAttitude = sumAttitude + (pmtSubSocialParams[i].welfare + 
+                                             pmtSubSocialParams[i].trustInAuthorities + 
+                                             pmtSubSocialParams[i].agriculturalExperience)/3
+                sumSubjectiveNorm = sumSubjectiveNorm + (pmtSubSocialParams[i].education + 
+                                                        pmtSubSocialParams[i].importanceAndUnderstandingOfConsequencesOfSubsidence + 
+                                                        pmtSubSocialParams[i].groundwaterStatus)/3
+                sumPreceivedBehavioralControl = sumPreceivedBehavioralControl + (pmtSubSocialParams[i].age
+                                                                                 + pmtSubSocialParams[i].education
+                                                                                 + pmtSubSocialParams[i].agriculturalExperience
+                                                                                 + pmtSubSocialParams[i].dependenceOnAgriculturalLivelihoods)/4
+            self.attitude = sumAttitude/len(pmtSubSocialParams)
+            self.subjectiveNorm = sumSubjectiveNorm/len(pmtSubSocialParams)
+            self.preceivedBehavioralControl = sumPreceivedBehavioralControl/len(pmtSubSocialParams)
+            self.pmtSocialFactor = (self.attitude + self.subjectiveNorm + self.preceivedBehavioralControl) / 3
+
+class SCTSocialParameters:
+        performanceAccomplishment = 0.0
+        emotionalArousal = 0.0
+        VicariousLearning = 0.0
+        socialPersuation = 0.0
+        # results:
+        approachAndAvoidance = 0.0
+        performance = 0.0
+        persistence = 0.0
+        
+        
+
 class Agent:
     agentType = 1  # 1: proactive, 2: interactive, 3: bounded rational, 4: preceptive
     typeOfUsing = 1 # 1: agricultural, 2: industrial, 3: municipal
@@ -70,6 +115,7 @@ class Environment:
         self.effectOnType1 = None
         self.effectOnType2 = None
         self.effectOnType3 = None
+        self.effectOnType4 = None
 
     def set_well_coordinates(self, coordinates):
         """Sets coordinates for all wells."""
@@ -79,11 +125,12 @@ class Environment:
         """Add a single well's coordinates."""
         self.wellsCoordinates.append({"id": well_id, "x": x, "y": y})
 
-    def set_effects(self, effectOnType1, effectOnType2, effectOnType3):
+    def set_effects(self, effectOnType1, effectOnType2, effectOnType3, effectOnType4):
         """Set the effects of groundwater usage on different user types."""
         self.effectOnType1 = effectOnType1
         self.effectOnType2 = effectOnType2
         self.effectOnType3 = effectOnType3
+        self.effectOnType4 = effectOnType4
 
     def update_social_condition(self, new_condition):
         """Update the social condition based on usage patterns."""
@@ -99,7 +146,8 @@ class Environment:
         print(f"Wells Coordinates: {self.wellsCoordinates}")
         print(f"Effect on Type1: {self.effectOnType1}")
         print(f"Effect on Type2: {self.effectOnType2}")
-        print(f"Effect on Excessive Users: {self.effectOnType3}")
+        print(f"Effect on Type3: {self.effectOnType3}")
+        print(f"Effect on Type4: {self.effectOnType4}")
 
     def __str__(self):
         return (f"Environment {self.name}: {self.socialCondition} - {self.groundWaterHeight}\
@@ -109,11 +157,12 @@ class Environment:
 # 4. Scenarios
 # 4.1. define scenario
 class Scenario:
-    def __init__(self, name, effectOnType1, effectOnType2, effectOnType3):
+    def __init__(self, name, effectOnType1, effectOnType2, effectOnType3, effectOnType4):
         self.name = name
         self.effectOnType1 = effectOnType1
         self.effectOnType2 = effectOnType2
         self.effectOnType3 = effectOnType3
+        self.effectOnType4 = effectOnType4
 
 # 4.2. test scenario
 # 4.2.1. implementing scenario on the environment and agents
@@ -155,7 +204,7 @@ def calculateSocialCondition(agents):
     rounded_average = round(average)  # Round to nearest whole number
     return rounded_average
 
-scenario1 = Scenario("encouragement", 1, 1, 0.5)
+scenario1 = Scenario("encouragement", 1, 1, 0.5, 1)
 
 agents = makeAPopulation(10)
 print(123)
